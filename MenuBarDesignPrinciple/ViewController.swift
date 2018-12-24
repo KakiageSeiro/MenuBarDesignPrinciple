3aae3c1eca209749758f50564826c2e682f11138
YutaEbisuno <frederika07_bernkastel@yahoo.co.jp>
2018-12-24 21:43:10 +0900

    Initial Commit

---
+++ MenuBarDesignPrinciple/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuBarDesignPrinciple
+//
+//  Created by KakiageSeiro on 2018/12/24.
+//  Copyright © 2018 kakiage.seiro. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
